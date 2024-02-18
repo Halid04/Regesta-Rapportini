0c861730c77b993c4c7fdf3d733de4956b4385b0
Halid Cisse <77756563+Halid04@users.noreply.github.com>
2024-02-18 15:53:33 +0100

    ADD: Aggiunta campi mancanti per creazione tickets

--- app/controller/CreazioneTickets.controller.js
+++ app/controller/CreazioneTickets.controller.js
@@ -13,6 +13,7 @@ sap.ui.define(
     let IDTicketsCreati = [];
     let clienteID = null;
     let commessaID = null;
+    let tipologiaID = null;
     return BaseController.extend("rapportini.controller.CreazioneTickets", {
       generateIDTickets: function () {
         return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
@@ -38,42 +39,58 @@ sap.ui.define(
       creaModelloVuoto: function () {
         return {
           modelloTicket: {
-            utente: "",
+            insertDate: new Date(),
+            utente: this.getView()
+              .getModel("globalData")
+              .getProperty("/myUsername"),
             areaFunzionale: "",
             titolo: "",
             testo: "",
+            giorniStima: 0.0,
             cliente: null,
             commessa: null,
             assegnatoA: "",
+            giorniStimaDev: 0.0,
+            giorniStimaFunz: 0.0,
             criticita: "",
+            supportoFunzionale: "",
             dataConsegnaRichiesta: null,
             status: 0,
-            dataConsegnaSchedulata: null,
+            // dataConsegnaSchedulata: null,
             allegato: "",
+            externalID: "",
             flagVisibileCliente: false,
             flagBugFix: false,
             ultimaModifica: null,
+            tipologia: null,
           },
         };
       },
       creaModelloEsistente: function (ticket) {
         return {
           modelloTicket: {
+            insertDate: new Date(ticket.insertDate),
             utente: ticket.utente,
             areaFunzionale: ticket.areaFunzionale,
             titolo: ticket.titolo,
             testo: ticket.testo,
+            giorniStima: ticket.giorniStima,
             cliente: ticket.IDCliente_ID,
             commessa: ticket.IDCommessa_ID,
             assegnatoA: ticket.assegnatoA,
+            giorniStimaDev: ticket.giorniStimaDev,
+            giorniStimaFunz: ticket.giorniStimaFunz,
             criticita: ticket.criticita,
+            supportoFunzionale: ticket.supportoFunzionale,
             dataConsegnaRichiesta: new Date(ticket.dataConsegnaRichiesta),
             status: ticket.status,
-            dataConsegnaSchedulata: new Date(ticket.dataConsegnaSchedulata),
+            // dataConsegnaSchedulata: new Date(ticket.dataConsegnaSchedulata),
             allegato: ticket.allegato,
+            externalID: ticket.externalID,
             flagVisibileCliente: ticket.flagVisibileCliente,
             flagBugFix: ticket.flagBugFix,
             ultimaModifica: ticket.ultimaModifica,
+            tipologia: ticket.IDTipologia_ID,
           },
         };
       },
@@ -96,14 +113,14 @@ sap.ui.define(
                 .bindList("/Tickets")
                 .requestContexts();
               var tickets = contexts.map((x) => x.getObject());
-              
+
               let value = tickets.find((element) => {
                 return element.ID === IDCurrent;
               });
 
               let index = tickets.indexOf(value);
               console.log("index", index);
-              
+
               oData = this.creaModelloEsistente(tickets[index]);
             }
 
@@ -119,21 +136,28 @@ sap.ui.define(
           var path = "/Tickets(" + IDCurrent + ")";
 
           const properties = [
+            "insertDate",
             "utente",
             "areaFunzionale",
             "titolo",
             "testo",
+            "giorniStima",
             "IDCliente_ID",
             "IDCommessa_ID",
             "assegnatoA",
+            "giorniStimaDev",
+            "giorniStimaFunz",
             "criticita",
+            "supportoFunzionale",
             "dataConsegnaRichiesta",
             "status",
-            "dataConsegnaSchedulata",
+            // "dataConsegnaSchedulata",
             "allegato",
+            "externalID",
             "flagVisibileCliente",
             "flagBugFix",
             "ultimaModifica",
+            "IDTipologia_ID",
           ];
 
           for (var i = 0; i < properties.length; i++) {
@@ -147,38 +171,66 @@ sap.ui.define(
         oDataModel.submitBatch("myAppUpdateGroup");
         myRouter.navTo("tickets");
       },
-      handleSelectionChangeCliente: async function(oEvent) {
+      handleSelectionChangeCliente: async function (oEvent) {
         let selectedKeys = oEvent.getSource().getSelectedKey();
         clienteID = selectedKeys;
-    
-        if(commessaID != null){
+
+        if (commessaID != null) {
           commessaID = null;
-          MessageBox.show("Cliente cambiato, commessa resettata");
+          // MessageBox.show("Cliente cambiato, commessa resettata");
           this.getView().byId("comboCommesse").setSelectedKey(null);
         }
 
-        var commesseContexts = await this.getView().getModel().bindList("/Commesse").requestContexts();
-        var commesseList = []
+        if (tipologiaID != null) {
+          tipologiaID = null;
+          // MessageBox.show("Cliente cambiato, Tipologia resettata");
+          this.getView().byId("comboTipologie").setSelectedKey(null);
+        }
+
+        var commesseContexts = await this.getView()
+          .getModel()
+          .bindList("/Commesse")
+          .requestContexts();
+        var commesseList = [];
         var commesse = commesseContexts.map((x) => x.getObject());
-        commesse.forEach(commessa => {
-          if(commessa.IDCliente_ID == clienteID){
+        commesse.forEach((commessa) => {
+          if (commessa.IDCliente_ID == clienteID) {
             commesseList.push({
               ID: commessa.ID,
-              descrizione: commessa.descrizione
-            })
+              descrizione: commessa.descrizione,
+            });
           }
         });
-    
-        console.log(commesseList)
+
+        var tipologiaContexts = await this.getView()
+          .getModel()
+          .bindList("/Tipologia")
+          .requestContexts();
+        var tipologiaList = [];
+        var tipologia = tipologiaContexts.map((x) => x.getObject());
+        tipologia.forEach((tipologia) => {
+          if (tipologia.IDCliente_ID == clienteID) {
+            tipologiaList.push({
+              ID: tipologia.ID,
+              tipologia: tipologia.tipologia,
+            });
+          }
+        });
+
+        // console.log(commesseList);
         var commesseModel = new JSONModel(commesseList);
         this.getView().setModel(commesseModel, "Commesse");
-    },
-      handleSelectionChangeCommessa: async function(oEvent) {
+        var tipologiaModel = new JSONModel(tipologiaList);
+        this.getView().setModel(tipologiaModel, "Tipologia");
+      },
+      handleSelectionChangeCommessa: async function (oEvent) {
         let selectedKeys = oEvent.getSource().getSelectedKey();
         commessaID = selectedKeys;
-
       },
-      
+      handleSelectionChangeTipologia: async function (oEvent) {
+        let selectedKeys = oEvent.getSource().getSelectedKey();
+        tipologiaID = selectedKeys;
+      },
       onSave: async function () {
         function dateFormater(date) {
           if (date) {
@@ -196,26 +248,33 @@ sap.ui.define(
           .getModel("JSONModel")
           .getProperty("/modelloTicket");
 
-          const campiObbligatori = ["utente", "cliente", "commessa"];
+        const campiObbligatori = [
+          "utente",
+          "cliente",
+          "commessa",
+          "areaFunzionale",
+          "titolo",
+          "tipologia",
+          "dataConsegnaRichiesta",
+        ];
 
         for (var i = 0; i < campiObbligatori.length; i++) {
           if (
             modelloTicket[campiObbligatori[i]] == "" ||
             modelloTicket[campiObbligatori[i]] == null
           ) {
-            console.log(campiObbligatori[i])
-            console.log(modelloTicket[campiObbligatori[i]])
-            MessageBox.show("Per favore, compila tutti i campi obbligatori");
+            // console.log(campiObbligatori[i]);
+            // console.log(modelloTicket[campiObbligatori[i]]);
+            MessageToast.show("Per favore, compila tutti i campi obbligatori");
             return;
           }
         }
-      
 
         const currentDate = new Date();
 
         let newTicket = {
           ID: this.onCheckIDTickets(),
-          insertDate: dateFormater(currentDate),
+          insertDate: dateFormater(modelloTicket.insertDate),
           utente: modelloTicket.utente,
           IDCliente_ID: parseInt(clienteID),
           IDCommessa_ID: parseInt(commessaID),
@@ -223,21 +282,22 @@ sap.ui.define(
           titolo: modelloTicket.titolo,
           testo: modelloTicket.testo,
           propostoA: "",
-          giorniStima: 0.0,
+          giorniStima: modelloTicket.giorniStima,
           dataConsegnaRichiesta: dateFormater(
             modelloTicket.dataConsegnaRichiesta
           ),
           assegnatoA: modelloTicket.assegnatoA,
           giorniCons: 0.0,
-          dataConsegnaSchedulata: dateFormater(
-            modelloTicket.dataConsegnaSchedulata
-          ),
+          // dataConsegnaSchedulata: dateFormater(
+          //   modelloTicket.dataConsegnaSchedulata
+          // ),
+          dataConsegnaSchedulata: null,
           status: parseInt(modelloTicket.status),
           dataChiusura: null,
           ordinamento: 0,
           allegato: modelloTicket.allegato,
           statusPrev: 0,
-          externalID: null,
+          externalID: modelloTicket.externalID,
           flagVisibileCliente: modelloTicket.flagVisibileCliente,
           dataProduzione: null,
           flagBugFix: modelloTicket.flagBugFix,
@@ -250,8 +310,8 @@ sap.ui.define(
           chatPrivata: "",
           dataSpecifiche: null,
           giorniConsDev: 0.0,
-          giorniStimaDev: 0.0,
-          giorniStimaFunz: 0.0,
+          giorniStimaDev: modelloTicket.giorniStimaDev,
+          giorniStimaFunz: modelloTicket.giorniStimaFunz,
           giorniConsFunz: 0.0,
           dataSviluppi: null,
           flagDev: false,
@@ -260,7 +320,7 @@ sap.ui.define(
           flagPadre: false,
           flagFiglio: false,
           nRilavorazioni: 0,
-          supportoFunzionale: "",
+          supportoFunzionale: modelloTicket.supportoFunzionale,
           flagNeedDev: false,
           flagNeedFunz: false,
           flagIngegnerizzabile: false,
@@ -271,11 +331,11 @@ sap.ui.define(
           ultimaModificaCliente: null,
           ultimaModificaUtenteCliente: null,
           flagAms: false,
-          IDTipologia: null,
+          IDTipologia_ID: parseInt(tipologiaID),
           inoltraA: "",
           messageID: "",
         };
-        console.log(clienteID)
+        console.log(clienteID);
         console.log("ticket da salvare", newTicket);
         let oDataModel = this.getView().getModel();
         let oBinding = await oDataModel.bindList("/Tickets");
